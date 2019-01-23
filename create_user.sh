5c8b902753ea4aa805ffcdb5c0e7f19407eab20c
Pedro Andrade <pedromca89@gmail.com>
2019-01-23 12:39:09 +0000

    1.0

---
+++ create_user.sh
@@ -0,0 +1,19 @@
+
+
+
+sudo useradd pedro
+sudo passwd pedro
+
+sudo usermod -aG wheel pedro
+
+sudo vi /etc/sudoers
+
+sudo chmod 755 /etc/sudoers
+sudo vi /etc/sudoers
+
+sudo chmod 440 /etc/sudoers
+
+sudo vi /etc/ssh/sshd_config
+
+sudo service sshd restart
+                            
