7cd6b59f8535e97ddeddab7719cf8510bc0d4828
Pedro Andrade <pedromca89@gmail.com>
2019-06-12 13:41:19 +0100

    shell script distribute public key to remote machines

---
+++ passwordless_ssh.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+PUB_KEY=$1
+HOST_LIST=$2
+ID_RSA=`cat ~/.ssh/id_rsa.pub` 
+
+set_ssh() {
+        while read  line;
+	do
+                echo "Copying id_rsa pub key to $line authorized_keys..."
+                ssh -i $PUB_KEY centos@$line "echo $ID_RSA >> ~/.ssh/authorized_keys" < /dev/null
+        done < $HOST_LIST
+}
+
+
+if [[ $# -lt 2 ]]; then
+	echo "Missing arguments! Usage : './passwordless_ssh.sh public_key_file hosts_list_file'"
+elif [[ $# -gt 2 ]]; then
+	echo "Missing arguments! Usage : './passwordless_ssh.sh public_key_file hosts_list_file'"
+else
+	set_ssh
+fi
